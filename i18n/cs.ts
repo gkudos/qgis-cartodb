f6ebf21c61d106f01ebf9d8e51fd0d8d3c961066
Jiří Podhorecký <trendspotter@users.noreply.github.com>
2023-06-24 23:01:34 +0200

    Add Czech language
    export from Transifex

---
+++ i18n/cs.ts
@@ -0,0 +1,317 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="cs" version="2.0">
+    <context>
+        <name>CartoDBPlugin</name>
+        <message>
+            <!-- <location filename="test_translations.py" line="48"/> -->
+            <source>Add CARTO Layer</source>
+            <translation>Přidat vrstvu CARTO</translation>
+        </message>
+        <message>
+            <source>Add SQL CARTO Layer</source>
+            <translation>Přidat vrstvu SQL CARTO</translation>
+        </message>
+        <message>
+            <source>Upload layers to CARTO</source>
+            <translation>Nahrát vrstvy do CARTO</translation>
+        </message>
+        <message>
+            <source>Create New Map</source>
+            <translation>Vytvořit novou mapu</translation>
+        </message>
+        <message>
+            <source>CARTO Layers</source>
+            <translation>CARTO Vrstvy</translation>
+        </message>
+        <message>
+            <source>Filter Tables</source>
+            <translation>Filtr tabulek</translation>
+        </message>
+        <message>
+            <source>Download data only within the current view</source>
+            <translation>Stáhnout data pouze v aktuálním zobrazení</translation>
+        </message>
+        <message>
+            <source>Name</source>
+            <translation>Název</translation>
+        </message>
+        <message>
+            <source>{}, using {} of {}</source>
+            <translation>{}, pomocí {} z {}</translation>
+        </message>
+        <message>
+            <source>{}, using {} of {}, {} tables, {} shared</source>
+            <translation>{}, pomocí {} z {}, {} tabulek, {} sdílených</translation>
+        </message>
+        <message>
+            <source>Uploading {}</source>
+            <translation>Nahrávání {}</translation>
+        </message>
+        <message>
+            <source>Upload Complete</source>
+            <translation>Nahrávání dokončeno</translation>
+        </message>
+        <message>
+            <source>Error uploading layer: {}</source>
+            <translation>Chyba při nahrávání vrstvy: {}</translation>
+        </message>
+        <message>
+            <source>Using {} of {}</source>
+            <translation>Pomocí {} z {}</translation>
+        </message>
+        <message>
+            <source>Warning</source>
+            <translation>Upozornění</translation>
+        </message>
+        <message>
+            <source>Processed {} %</source>
+            <translation>Zpracováno {} %</translation>
+        </message>
+        <message>
+            <source>Processed {}% - Loaded: {}</source>
+            <translation>Zpracováno {}% - Načteno: {}</translation>
+        </message>
+        <message>
+            <source>Downloading datasets</source>
+            <translation>Stažení datových sad</translation>
+        </message>
+        <message>
+            <source>Save connection</source>
+            <translation>Uložit spojení</translation>
+        </message>
+        <message>
+            <source>Both User and Api Key must be provided</source>
+            <translation>Musí být k dispozici Uživatelský i Api klíč</translation>
+        </message>
+        <message>
+            <source>Overwrite {}?</source>
+            <translation>Přepsat {}?</translation>
+        </message>
+        <message>
+            <source>Creating Map</source>
+            <translation>Vytváření mapy</translation>
+        </message>
+        <message>
+            <source>Map Created</source>
+            <translation>Mapa vytvořena</translation>
+        </message>
+        <message>
+            <source>{} created</source>
+            <translation>{} vytvořeno</translation>
+        </message>
+        <message>
+            <source>{} created, but has warnings</source>
+            <translation>{} vytvořeno, ale s varováním</translation>
+        </message>
+        <message>
+            <source>All layers are SQL layers</source>
+            <translation>Všechny vrstvy jsou vrstvy SQL</translation>
+        </message>
+        <message>
+            <source>At least one CARTO layer should belong or be visible to {}</source>
+            <translation>Nejméně jedna vrstva CARTO by měla patřit nebo být viditelná pro {}</translation>
+        </message>
+        <message>
+            <source>There should be at least one CARTO layer in the project.</source>
+            <translation>V projektu by měla být alespoň jedna vrstva CARTO.</translation>
+        </message>
+        <message>
+            <source>Copy Link</source>
+            <translation>Kopírovat odkaz</translation>
+        </message>
+        <message>
+            <source>Open</source>
+            <translation>Otevřít</translation>
+        </message>
+        <message>
+            <source>Add CARTO Connection</source>
+            <translation>Přidat připojení CARTO</translation>
+        </message>
+        <message>
+            <source>Remove connection {}?</source>
+            <translation>Odpojit připojení {}?</translation>
+        </message>
+        <message>
+            <source>Confirm delete</source>
+            <translation>Potvrdit smazání</translation>
+        </message>
+        <message>
+            <source>Error loading data from CARTO</source>
+            <translation>Při načítání dat z karty CARTO došlo k chybě</translation>
+        </message>
+        <message>
+            <source>Error getting tables</source>
+            <translation>Chyba při získávání tabulek</translation>
+        </message>
+        <message>
+            <source>Validating Query</source>
+            <translation>Ověření dotazu</translation>
+        </message>
+        <message>
+            <source>Query is valid</source>
+            <translation>Dotaz je platný</translation>
+        </message>
+    </context>
+    <context>
+        <name>NewConnection</name>
+        <message>
+            <source>New CARTO Connection</source>
+            <translation>Nové připojení CARTO</translation>
+        </message>
+        <message>
+            <source>New Connection</source>
+            <translation>Nové připojení</translation>
+        </message>
+        <message>
+            <source>CARTO User</source>
+            <translation>CARTO Uživatel</translation>
+        </message>
+        <message>
+            <source>CARTO API Key:</source>
+            <translation>Klíč API CARTO:</translation>
+        </message>
+        <message>
+            <source>Multiuser Account?</source>
+            <translation>Víceuživatelský účet?</translation>
+        </message>
+    </context>
+    <context>
+        <name>ConnectionManager</name>
+        <message>
+            <source>Connection Manager</source>
+            <translation>Správce připojení</translation>
+        </message>
+        <message>
+            <source>Select Connection</source>
+            <translation>Vybrat Připojení</translation>
+        </message>
+        <message>
+            <source>New</source>
+            <translation>Nový</translation>
+        </message>
+        <message>
+            <source>Edit</source>
+            <translation>Upravit</translation>
+        </message>
+        <message>
+            <source>Delete</source>
+            <translation>Smazat</translation>
+        </message>
+        <message>
+            <source>Connect</source>
+            <translation>Připojit</translation>
+        </message>
+    </context>
+    <context>
+        <name>Upload</name>
+        <message>
+            <source>Upload layers to CARTO</source>
+            <translation>Nahrát vrstvy do CARTO</translation>
+        </message>
+        <message>
+            <source>Project Layers</source>
+            <translation>Projektové vrstvy</translation>
+        </message>
+        <message>
+            <source>Replace if exists</source>
+            <translation>Nahradit, pokud existuje</translation>
+        </message>
+        <message>
+            <source>Add CARTO layer to project</source>
+            <translation>Přidat vrstvu CARTO do projektu</translation>
+        </message>
+        <message>
+            <source>Cancel</source>
+            <translation>Zrušit</translation>
+        </message>
+        <message>
+            <source>Upload</source>
+            <translation>Nahrát</translation>
+        </message>
+        <message>
+            <source>Ready</source>
+            <translation>Připraveno</translation>
+        </message>
+    </context>
+    <context>
+        <name>CreateViz</name>
+        <message>
+            <source>Create New Map</source>
+            <translation>Vytvořit novou mapu</translation>
+        </message>
+        <message>
+            <source>Map Name:</source>
+            <translation>Název mapy:</translation>
+        </message>
+        <message>
+            <source>Description</source>
+            <translation>Popis</translation>
+        </message>
+        <message>
+            <source>Map Name</source>
+            <translation>Název mapy</translation>
+        </message>
+        <message>
+            <source>Available Layers</source>
+            <translation>Dostupné vrstvy</translation>
+        </message>
+        <message>
+            <source>Map Layers:</source>
+            <translation>Mapa vrstev:</translation>
+        </message>
+        <message>
+            <source>Avatar</source>
+            <translation>Avatar</translation>
+        </message>
+        <message>
+            <source>Name</source>
+            <translation>Název</translation>
+        </message>
+        <message>
+            <source>SQL</source>
+            <translation>SQL</translation>
+        </message>
+        <message>
+            <source>Cancel</source>
+            <translation>Zrušit</translation>
+        </message>
+        <message>
+            <source>Create</source>
+            <translation>Vytvořit</translation>
+        </message>
+        <message>
+            <source>Public</source>
+            <translation>Veřejný</translation>
+        </message>
+    </context>
+    <context>
+        <name>NewSQL</name>
+        <message>
+            <source>Add CARTO SQL Layer</source>
+            <translation>Přidat vrstvu CARTO SQL</translation>
+        </message>
+        <message>
+            <source>SQL Query</source>
+            <translation>SQL Query</translation>
+        </message>
+        <message>
+            <source>Load Tables</source>
+            <translation>Načíst tabulky</translation>
+        </message>
+        <message>
+            <source>Tables</source>
+            <translation>Tabulky</translation>
+        </message>
+        <message>
+            <source>Cancel</source>
+            <translation>Zrušit</translation>
+        </message>
+        <message>
+            <source>Test Query</source>
+            <translation>Test Query</translation>
+        </message>
+        <message>
+            <source>Add Layer</source>
+            <translation>Přidat vrstvu</translation>
+        </message>
+    </context>
+</TS>
